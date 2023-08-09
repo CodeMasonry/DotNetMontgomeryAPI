6d7d0270da328bd9a995f29d389f9be18fa26c0b
Seth <sethwhite456@outlook.com>
2023-08-08 19:36:33 -0500

    Added registration models

---
+++ Dtos/UserForLoginDto.cs
@@ -0,0 +1,9 @@
+namespace DotNetAPI.Dto
+{
+    public partial class UserForLoginDto
+    {
+        public string UserName { get; set; } = ""; //needs further implementation
+        public string Email {get; set;} = "";
+        public string Password {get; set;} = "";
+    }
+}
